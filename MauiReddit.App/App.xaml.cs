19874b0ae512f39bb9517f56fdc085765175827b
Felipe Santiago <felipe.santiago@outlook.com>
2024-02-07 18:50:07 +0100

    Adding .netmaui solution, and moving main screen to views folder.

---
+++ MauiReddit.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiReddit.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiReddit.App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiReddit.App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
